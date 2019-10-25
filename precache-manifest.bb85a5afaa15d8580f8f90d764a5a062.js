03472b30ae36b565ad851405677c6debf6570408
zohar <zohar@Andrews-MacBook-Pro.local>
2019-10-24 21:15:51 -0600

    Updates

---
+++ precache-manifest.bb85a5afaa15d8580f8f90d764a5a062.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2f52eca08cca7e046b82f6e37ae3de",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "eb7fca2c3618ad28afbb",
+    "url": "/clicky-game/static/css/main.73f1666c.chunk.css"
+  },
+  {
+    "revision": "8ddf23b8ae8e7f5c8847",
+    "url": "/clicky-game/static/js/2.19c06209.chunk.js"
+  },
+  {
+    "revision": "eb7fca2c3618ad28afbb",
+    "url": "/clicky-game/static/js/main.32252531.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
